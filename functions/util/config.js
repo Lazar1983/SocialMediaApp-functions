ea3fa5cd634d70e97ce98967b986b8dd1c96c5c1
Lazar <lazov_lazar@yahoo.com>
2020-04-11 23:26:41 +0200

    refactoring and organizing code

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyB8cbTPR99myU2pqorq45p2Wu_FYXitjZY",
+    authDomain: "socialmediaapp-4cc1a.firebaseapp.com",
+    databaseURL: "https://socialmediaapp-4cc1a.firebaseio.com",
+    projectId: "socialmediaapp-4cc1a",
+    storageBucket: "socialmediaapp-4cc1a.appspot.com",
+    messagingSenderId: "540276483133",
+    appId: "1:540276483133:web:75d581f6afb4af1f9f3654",
+    measurementId: "G-VMQ9BWK9NK"
+};
