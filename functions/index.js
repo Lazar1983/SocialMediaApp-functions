e3844c32921c4837634336932e8ed1ee0ef29450
Lazar <lazov_lazar@yahoo.com>
2020-04-12 21:43:08 +0200

    added user details

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const app = require('express')();
 const FBAuth = require('./util/fbAuth')
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage } = require('./handlers/users');
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
 //screams routes
 app.get('/screams', getAllScreams);
@@ -14,7 +14,9 @@ app.post('/scream', FBAuth, postOneScream);
 //users routes
 app.post('/signup', signup);
 app.post('/login', login);
-app.post('/user/image', uploadImage);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
  
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
