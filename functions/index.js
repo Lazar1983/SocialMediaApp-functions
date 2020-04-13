757054aed1838760573e41b0faf598ec096f2da1
Lazar <lazov_lazar@yahoo.com>
2020-04-13 21:07:13 +0200

    fetching one scream, and cooment

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,27 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth')
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
+const { 
+  getAllScreams, 
+  postOneScream, 
+  getScream, 
+  commentOnScream 
+} = require('./handlers/screams');
+
+const { 
+  signup, 
+  login, 
+  uploadImage, 
+  addUserDetails, 
+  getAuthenticatedUser 
+} = require('./handlers/users');
 
 //screams routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 //users routes
 app.post('/signup', signup);
